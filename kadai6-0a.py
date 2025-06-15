8bc0c4066f29db96595b3bd12914be1cb68ce2d9
Nuriwasabi Shimeskier <j23308nt@edu.tuis.ac.jp>
2025-06-16 07:08:31 +0900

    6a

--- kadai6-0a.py
+++ kadai6-0a.py
@@ -1,19 +1,11 @@
 import requests
 
 APP_ID = "77435cb40c39a246815b03bffba944bfea4f9ea3"
-API_URL  = "https://api.e-stat.go.jp/rest/3.0/app/json/getStatsData"
+API_URL  = "http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData?appId=&lang=J&statsDataId=0002119864&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0"
 
 params = {
     "appId": APP_ID,
-    "statsDataId":"0000020201",
-    "cdArea":"12101,12102,12103,12104,12105,12106",
-    "cdCat01": "A1101",
-    "metaGetFlg":"Y",
-    "cntGetFlg":"N",
-    "explanationGetFlg":"Y",
-    "annotationGetFlg":"Y",
-    "sectionHeaderFlg":"1",
-    "replaceSpChars":"0",
+    "statsDataId":"00500509",
     "lang": "J"  # 日本語を指定
 }
 
